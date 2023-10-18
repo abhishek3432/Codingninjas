aee3b6bdcbeb96eff64080d4012a7b5d83d358a1
git config --global user.name Abhisek <akt343243@gmail.com>
2023-10-18 06:54:30 +0530

    main

---
+++ Sum Of Max And Min.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+int sumOfMaxMin(int arr[], int n){
+	// Write your code here.
+	int max=INT_MIN, min=INT_MAX;
+	for(int i=0; i<n; i++)
+	{
+		if(arr[i]>=max)
+			max=arr[i];
+		if(arr[i]<=min)
+			min=arr[i];
+
+	}
+
+	return max+min;
+}
