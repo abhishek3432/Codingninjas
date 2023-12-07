492561154bf8df3d44644d2277d7c8be5a12de4f
Abhishek Tiwari <akt343243@gmail.com>
2023-12-07 13:45:58 +0530

    commit

---
+++ Data Type.cpp
@@ -0,0 +1,15 @@
+int dataTypes(string type) {
+	// Write your code here
+	if(type=="Integer")
+		return 4;
+	if(type=="Long")
+		return 8;
+	if(type=="Double")
+		return 8;
+	if(type=="Float")
+		return 4;
+	if(type=="Character")
+		return 1;
+
+	
+}
