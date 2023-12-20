33abacd7db5ed87e96b99b48b4874e1e76ea730c
Abhishek Tiwari <akt343243@gmail.com>
2023-12-20 21:32:04 +0530

    update

---
+++ Check Prime.cpp
@@ -0,0 +1,22 @@
+bool isPrime(int n)
+{
+	int count=0;
+
+    for(int i=1;i*i<=n;i++){
+
+        if(n%i==0){
+
+            count++;    
+
+            if(n/i!=i) count++;
+
+        }
+
+    }
+
+    if(count==2) return true;
+
+    else
+
+     return false;
+}
