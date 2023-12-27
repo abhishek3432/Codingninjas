b5da5b0a98e0f0e031c1705cde65aa8a5eee3a12
Abhishek Tiwari <akt343243@gmail.com>
2023-12-27 12:28:56 +0530

    commit ho ja

---
+++ Introduction To Linked List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition of linked list
+ * class Node {
+ *
+ * public:
+ *     int data;
+ *     Node* next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node* next) : data(x), next(next) {}
+ * };
+ */
+
+
+Node* constructLL(vector<int>& arr) {
+    // Write your code here
+    Node* head= new Node(arr[0]);
+    Node* temp=head;
+    
+    for(int i=1; i<arr.size(); i++)
+    {
+    Node* newNode= new Node(arr[i]);
+    temp->next= newNode;
+    temp= newNode;
+    }
+    return head;
+}
