5b7c666e1796fc895f6452bebf5ee81d98a2001a
Abhishek Tiwari <akt343243@gmail.com>
2023-12-30 10:24:55 +0530

    commit ho ja simsim

---
+++ Count nodes of linked list.cpp
@@ -0,0 +1,46 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+int length(Node *head)
+{
+	//Write your code here
+    
+    Node* temp= head;
+    int length=0;
+    while(temp!=NULL)
+    {
+       
+        length++;
+        temp=temp->next;
+    }
+
+    return length;
+
+    
+}
