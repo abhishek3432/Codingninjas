06a66257245befbec8999383f900df68327dfce4
Abhishek Tiwari <akt343243@gmail.com>
2024-01-13 17:39:45 +0530

    commit

---
+++ Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,18 @@
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+	//two pointer approach
+        
+        int i=0;
+        for(int j=1; j<n; j++)
+        {
+            if(arr[j] != arr[i])
+            {
+                arr[i+1] = arr[j];
+                i++;
+            }
+                
+        }
+        
+        
+        return i+1;
+}
